9dca98e0806fb52d8787c24912307fcca4d7cc66
dannyling111 <64554169+dannyling111@users.noreply.github.com>
2023-01-31 18:09:03 +0800

    Add files via upload

--- temp-230129.py
+++ temp-230129.py
@@ -1,46 +1,24 @@
-
-import openai
+import yfinance as yf
 import streamlit as st
-
-# Get API key
-
-openai.organization = "org-ixe83K3WLxi5O4MhZwLa0ABR"
-openai.api_key = "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"
+import pandas as pd
 
 
-st.set_page_config(page_title="Resume Generator", page_icon=":guardsman:", layout="wide")
+st.write("""
+# Simple Stock Price App
 
-def generate_resume(prompt):
-    completions = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=1024)
-    message = completions.choices[0].text
-    return message.strip()
+Shown are the stock **closing price** and ***volume*** of Google!
 
-def main():
-    st.title("Resume Generator")
-    name = st.text_input("Name")
-    email = st.text_input("Email")
-    job_title = st.text_input("Job Title")
-    job_description = st.text_area("Job Description")
-    length = st.selectbox("Select Length", ["Short", "Medium", "Long"])
-    tone = st.selectbox("Select Tone", ["Formal", "Informal"])
-    if st.button("Generate Resume"):
-        if length == "Short":
-            prompt = (f"Write a short resume for {name} who is applying for the position of {job_title}. {job_description}")
-        elif length == "Medium":
-            prompt = (f"Write a medium resume for {name} who is applying for the position of {job_title}. {job_description}")
-        else:
-            prompt = (f"Write a long resume for {name} who is applying for the position of {job_title}. {job_description}")
-        if tone == "Formal":
-            prompt += " Use a formal tone."
-        else:
-            prompt += " Use an informal tone."
-        resume = generate_resume(prompt)
-        st.success("Resume Generated!")
-        st.write(resume)
-    if st.button("Download Resume"):
-        st.write("Downloading...")
-        st.file_downloader(resume, "resume.txt")
-        st.write("Download Complete!")
+""")
 
-if __name__== "__main__":
-    main()
+tickerDf=pd.DataFrame()
+
+tickerDf["Close"]=[1,2,4,5,6,7,8]
+tickerDf["Volume"]=[12,22,24,25,46,27,38]
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
