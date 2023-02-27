3ed4e7547671fe06968d5612dfc3a0d2773dcbe1
dannyling111 <64554169+dannyling111@users.noreply.github.com>
2023-02-27 16:54:53 +0800

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,7 +1,4 @@
 import streamlit as st
 
-# Everything is accessible via the st.secrets dict:
 
-st.write("stripe_key is:", st.secrets["stripe_key"])
-st.write("endpoint_key is :", st.secrets["endpoint_key"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+st.write("okkkk")
